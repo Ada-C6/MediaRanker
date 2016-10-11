c8be2b51c4e34276fdbfe887c5e20f441a3751c8
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-11 11:52:41 -0700

    migrated database

--- db/migrate/20161011183605_create_albums.rb
+++ db/migrate/20161011183605_create_albums.rb
@@ -1,8 +1,11 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
-
-      t.timestamps null: false
+        t.string :name
+        t.string :artist
+        t.string :description
+        t.integer :rank
+        t.timestamps null: false
     end
   end
 end
--- db/migrate/20161011183621_create_books.rb
+++ db/migrate/20161011183621_create_books.rb
@@ -1,8 +1,11 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-
-      t.timestamps null: false
+        t.string :name
+        t.string :author
+        t.string :description
+        t.integer :rank
+        t.timestamps null: false
     end
   end
 end
