0ac72ba7a02760120c5a355a65d569e185930fe6
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-11 12:14:52 -0700

    basic Movie index controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+movies = [
+  "Schindler's List", "The Lord of the Rings",
+  "Forrest Gump", "Life Is Beautiful",
+  "Interstellar", "Psycho",
+  "Raiders of the Lost Ark", "Rear Window",
+  "The Lion King", "Dr. Strangelove",
+  "WALL-E", "Monty Python and the Holy Grail"
+]
+
+directors =  [
+  "Abracadabra",
+  "Wil E Coyote",
+  "Homer Jay Simpson",
+  "Hilary Oss",
+]
+
+descriptions = %w(randomness insanity weirdness oddities)
+
+movies.each do |movie|
+  Movie.create(name: movie, director: directors.sample, description: descriptions.sample)
+end
