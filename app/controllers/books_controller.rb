e901bba88deb40b0e4e15ea65f514eb8256bb270
Nina Mutty <ninamutty@gmail.com>
2016-10-11 15:11:50 -0700

    created form and paths for book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   end
 
   def find_book
-    return Book.find(params[:vendor_id].to_i)
+    return Book.find(params[:id].to_i)
   end
 
   def show
@@ -12,15 +12,53 @@ class BooksController < ApplicationController
   end
 
   def new
+    @mybook = Book.new
+    @post_method = :post
+    @post_path = books_path
   end
 
   def create
+    @params = params
+    @mybook = Book.new
+    @mybook.title = params[:book][:title]
+    @mybook.author = params[:book][:author]
+    @mybook.genre = params[:book][:genre]
+    @mybook.description = params[:book][:description]
+    @mybook.rank = 0
+
+    if @mybook.save
+      redirect_to books_path
+    else
+      @error = "Did not save successfully. Try again. \nAll fields must be filled and address must be unique!"
+      @post_method = :post
+      @post_path = books_path
+      render :new_book
+    end
   end
 
   def edit
+    @mybook = find_book
+    @post_method = :put
+    @post_path = book_path(@mybook.id)
   end
 
   def update
+    @params = params
+    @mybook = find_book
+    @mybook.title = params[:book][:title]
+    @mybook.author = params[:book][:author]
+    @mybook.genre = params[:book][:genre]
+    @mybook.description = params[:book][:description]
+    # @mybook.rank 
+
+    if @mybook.save
+      redirect_to book_path(@mybook.id)
+    else
+      @error = "Did not save successfully. Try again. \nAll fields must be filled and address must be unique!"
+      @post_method = :put
+      @post_path = book_path(@mybook.id)
+      render :edit_book
+    end
   end
 
   def destroy
