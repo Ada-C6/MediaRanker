42c84048df7cdf89e8621bd5efd84e531bda3e06
Mary Morrison <marymorrison15@gmail.com>
2016-10-11 11:37:59 -0700

    populated arguments in all 3 controllers. Still need params

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,22 +1,42 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = Album.all
   end
 
   def new
+    @album = Album.new
   end
 
   def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to albums_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @album = Album.find(params[:id])
   end
 
   def update
+    @album = Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to albums_path
+    else
+      render :edit
+    end
+
   end
 
   def delete
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to album_path
   end
 
   def show
+    @album = Album.find(params[:id])
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,42 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
+    @movie = Movie.new(market_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @movie = Movie.find(params[:id])
   end
 
   def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+
   end
 
   def delete
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movie_path
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 end
