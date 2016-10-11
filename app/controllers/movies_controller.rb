8c1813b66f141aaf2b75355185acbadacdf130f3
Sarah Nahmias <nahmisa@gmail.com>
2016-10-11 12:31:18 -0700

    Update action so movie form works

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,12 +12,32 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+
+    end
+
+  end
+
 
   def destroy
     movie = Movie.find(params[:id])
     movie.destroy
     redirect_to movies_path
   end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :director, :description)
+  end
+
+
 
 
 end
