16f866806f5317ba6cf6bebd31cd71cfcb0f0139
guineveresaenger <guineveresaenger@yahoo.com>
2016-10-11 14:38:05 -0700

    Added views, new and edit function fo Book and Album controllers and views. Should include all back button options.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,22 +1,44 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = Album.all
   end
 
   def show
+    @album = Album.find(params[:id])
   end
 
   def new
+    @album = Album.new
   end
 
   def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to albums_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @album = Album.find(params[:id])
   end
 
   def update
+    @album = Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to album_path(@album.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :musician, :description, :vote)
   end
 end
