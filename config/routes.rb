eb65dedda3497b79faf94ffe120984e2d8aee5e3
Sarah Nahmias <nahmisa@gmail.com>
2016-10-11 10:58:34 -0700

    Adds books controller and resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :movies, except: [:new, :create]
+  resources :books, except: [:new, :create]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
