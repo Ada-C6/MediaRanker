ba308a537aec0f0adc548daccd12f949022ef571
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-11 14:29:34 -0700

    Built seed data generator with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 10.times do |i|
-  Album.create(name: Faker::Hipster.sentence(3))
+  Album.create(name: Faker::Hipster.sentence(3, false, 1).split.map(&:capitalize).join(' ').gsub(/\./, ""), # Hipster sentence with each word capitalized, period stripped from end of sentence
+  artist: Faker::Superhero.name,
+  description: "#{Faker::Hacker.adjective.capitalize} #{Faker::Music.instrument.downcase} #{Faker::Hipster.sentence(7, true, 3).downcase}",
+  ranking: rand(1..100))
+end
+
+10.times do |i|
+  Movie.create(name: "#{Faker::Name.first_name} #{Faker::Hacker.ingverb.capitalize} #{Faker::Space.star}",
+  director: "#{Faker::Pokemon.name} #{Faker::Name.last_name}",
+  description: "#{Faker::Company.catch_phrase}. #{Faker::Hipster.sentence(7, false, 6)} #{Faker::Hacker.say_something_smart}",
+  ranking: rand(1..100))
+end
+
+10.times do |i|
+  Book.create(name: "#{Faker::Book.genre.split.map(&:capitalize).join(' ')} #{Faker::Superhero.power} #{Faker::Music.instrument}", # Capitalized any words in book genre
+  author: Faker::Book.author,
+  description: "#{Faker::Beer.hop} #{Faker::StarWars.quote}",
+  ranking: rand(1..100))
 end
