52fd8c19f9fdab0f5468a4164f97424debf1cad8
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-11 14:17:47 -0700

    added ability to create a new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,19 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
