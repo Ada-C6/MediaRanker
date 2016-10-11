afb95689897b517e8064a031ba6a59fdfcee2793
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-11 14:47:06 -0700

    implemented new and edit controllers and views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,38 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movie_path
+    else
+      render :edit
+    end
+  end
+
+# ---- PRIVATE METHODS ----
+
+private
+
+def movie_params
+  params.require(:movie).permit(:name, :director, :description, :rank)
+end
 end
