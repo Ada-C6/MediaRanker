8a97a24f9aa85618a1fd066b3c146f37c01cec64
Melissa Rodriguez <olenellina@gmail.com>
2016-10-11 13:27:20 -0700

    adding models for Books, Albums and Movies

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161011202622) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "title"
+    t.string   "artist"
+    t.string   "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "director"
+    t.string   "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
