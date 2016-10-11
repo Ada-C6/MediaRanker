85d8eda158be63d32229b1afd65881cbdcee553f
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-11 16:20:15 -0700

    Coded all methods in the movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,15 +13,31 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(params[:id])
+    else
+      render :new
+    end
   end
 
   def edit
+    @movie = Movie.find(params[:id])
   end
 
   def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movie_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
   end
 
   def upvote
