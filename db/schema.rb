fa6afea9320b899a898ca59fd43a1c1d6add06ec
Qlwentt <qlwentt@gmail.com>
2016-10-11 13:54:00 -0700

    add album model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011204731) do
+ActiveRecord::Schema.define(version: 20161011205246) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
