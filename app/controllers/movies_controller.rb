343ec94c07a11c21da17d9d9b5ed8097c24ef134
Sarah Nahmias <nahmisa@gmail.com>
2016-10-11 12:18:49 -0700

    Adds destroy functionality to show movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,18 @@
 class MoviesController < ApplicationController
 
   def index
-  
+
   end
 
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def destroy
+    movie = Movie.find(params[:id])
+    movie.destroy
+    redirect_to movies_path
+  end
+
 
 end
