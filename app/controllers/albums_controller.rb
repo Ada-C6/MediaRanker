e64aff911e6133318510ad3bb16fe02432bff21e
Rachel <rachel.l.pavilanis@gmail.com>
2016-10-11 14:22:40 -0700

    added generic album form and rendered in edit/new.  Added code to albums controller for edit/new and update/create

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -12,15 +12,42 @@ class AlbumsController < ApplicationController
   end
 
   def edit
+    @album = findAlbum
+
+    if @album == nil
+          render :file => 'public/404.html',
+              :status => :not_found
+    end
   end
 
   def update
+    @album = findAlbum
+
+    if @album.update(title: params[:album][:title], recorded_by: params[:album][:recorded_by], description: params[:album][:description])
+      redirect_to action: "show"
+    else
+      render 'edit'
+    end
   end
 
   def new
+    @album = Album.new
   end
 
   def create
+    @params = params
+    @album = Album.new
+    @album.title = params[:album][:title]
+    @album.recorded_by = params[:album][:recorded_by]
+    @album.description = params[:album][:description]
+
+    if(@album.save)
+      #saved successfully; go to index
+      redirect_to :action => "index"
+    else
+      #validation failed; show the "new" form again
+      render :action => :new
+    end
   end
 
   def destroy
