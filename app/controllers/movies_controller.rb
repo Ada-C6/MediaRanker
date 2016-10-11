a28a44487fece3c14bd54d424c0406c4d07f3fb4
Qlwentt <qlwentt@gmail.com>
2016-10-11 16:58:20 -0700

    add edit/update and new/create functions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,15 +7,40 @@ class MoviesController < ApplicationController
   end
 
   def edit
+    @this_movie=Movie.find(params[:id])
+    @movie_method= :put
+    @movie_path= movie_url(@this_movie)
+    
   end
 
   def update
+    @this_movie=Movie.find(params[:id])
+    
+    @this_movie.name = params[:movie][:name]
+    @this_movie.director = params[:movie][:director]
+    @this_movie.description = params[:movie][:description]
+
+    @this_movie.save
+
+    redirect_to movie_url(@this_movie)
   end
 
   def new
+    @this_movie=Movie.new
+    @movie_method= :post
+    @movie_path= movie_url(@this_movie)
   end
 
   def create
+    @this_movie=Movie.new
+    #set the values for attributes
+    @this_movie.name = params[:movie][:name]
+    @this_movie.director = params[:movie][:director]
+    @this_movie.description = params[:movie][:description]
+    
+    @this_movie.save
+    
+    redirect_to movie_url(@this_movie)
   end
 
   def destroy
@@ -25,6 +50,5 @@ class MoviesController < ApplicationController
     @this_movie=Movie.find(params[:id])
     @this_movie.rank+=1
     @this_movie.save
-    redirect_to movie_url(@this_movie)
   end
 end
