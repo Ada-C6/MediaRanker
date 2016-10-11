4b8560372025fd59cdcb234c6b911dfb9b4007c5
Shari Meggs <smeggs1@gmail.com>
2016-10-11 14:14:57 -0700

    Set up the show, edit and index page for movies. No styling and basic funcationality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+  def index
+    @movie = Movie.all
+  end
+  def show
+    @movie = Movie.find(params[:id])
+  end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+  private
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :rank)
+  end
 end
