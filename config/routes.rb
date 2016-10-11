3e825db0d43492ba8b9da045cdab006667ccb09c
yaelittle <yaelittle@gmail.com>
2016-10-11 13:20:32 -0700

    controllers with 7 methods each

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'albums/index'
+
+  get 'albums/show'
+
+  get 'albums/new'
+
+  get 'albums/create'
+
+  get 'albums/edit'
+
+  get 'albums/update'
+
+  get 'albums/delete'
+
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'books/new'
+
+  get 'books/create'
+
+  get 'books/edit'
+
+  get 'books/update'
+
+  get 'books/delete'
+
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'movies/new'
+
+  get 'movies/create'
+
+  get 'movies/edit'
+
+  get 'movies/update'
+
+  get 'movies/delete'
+
   resources :albums
   resources :books
   resources :movies
@@ -57,4 +99,3 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
