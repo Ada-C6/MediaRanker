ac08ec1cf4a6981974c387f58e2aee186419adaa
Sarah Nahmias <nahmisa@gmail.com>
2016-10-11 11:23:34 -0700

    Fixes seeds file for missing commas and incorrect local variable name

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,31 +3,27 @@
 movies_seed = [
       {
         name: "Frozen",
-        director: "Disney"
+        director: "Disney",
         description: "Orphan sisters",
-        upvotes: 5,
-
+        upvotes: 5
       },
       {
         name: "Harry Potter",
-        director: "JK Rowling"
+        director: "JK Rowling",
         description: "It's also a movie!",
-        upvotes: 200,
-
+        upvotes: 200
       },
       {
         name: "Secret life of Pets",
-        director: "Animation Imagination!!!"
+        director: "Animation Imagination!!!",
         description: "Who let the dogs out?",
-        upvotes: 2,
-
+        upvotes: 2
       },
       {
         name: "A River Runs Through it",
-        director: "A Classic"
+        director: "A Classic",
         description: "Poor Brad Pitt",
-        upvotes: 0,
-
+        upvotes: 0
       },
     ]
 
@@ -40,31 +36,27 @@ end
 books_seed = [
       {
         name: "Lord of the Rings",
-        author: "JRR Tolkein"
+        author: "JRR Tolkein",
         description: "Hobits and rings",
-        upvotes: 5,
-
+        upvotes: 5
       },
       {
         name: "Harry Potter",
-        author: "JK Rowling"
+        author: "JK Rowling",
         description: "School and magic",
-        upvotes: 200,
-
+        upvotes: 200
       },
       {
         name: "Pat the Bunny",
-        author: "A children's book"
+        author: "A children's book",
         description: "Tactile reading!",
-        upvotes: 2,
-
+        upvotes: 2
       },
       {
         name: "Red Fish Blue Fish",
-        author: "Dr Suess"
+        author: "Dr Suess",
         description: "One Fish Two Fish!",
-        upvotes: 0,
-
+        upvotes: 0
       },
     ]
 
@@ -77,34 +69,30 @@ end
 albums_seed = [
       {
         name: "Lord of the Rings Soundtrack",
-        artist: "An Orchestra"
+        artist: "An Orchestra",
         description: "Hobits and rings and songs",
-        upvotes: 5,
-
+        upvotes: 5
       },
       {
         name: "Harry Potter",
-        artist: "Another Orchestra"
+        artist: "Another Orchestra",
         description: "School and magic... something wicked this way comes!",
-        upvotes: 200,
-
+        upvotes: 200
       },
       {
         name: "Ms Independent",
-        artist: "Destiny's Child"
+        artist: "Destiny's Child",
         description: "Beyonce was always a superstar",
-        upvotes: 2,
-
+        upvotes: 2
       },
       {
         name: "The Wheels on the Bus",
-        artist: "Mom"
+        artist: "Mom",
         description: "All through the town",
-        upvotes: 0,
-
+        upvotes: 0
       },
     ]
 
 albums_seed.each do |album|
-  Album.create book
+  Album.create album
 end
