ae9c3274428b7aad18cb3bba73a4e7a89da6e906
guineveresaenger <guineveresaenger@yahoo.com>
2016-10-11 12:05:30 -0700

    Added main view controller called Homes and also the main view

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,10 @@
 Rails.application.routes.draw do
-  get 'albums/index'
 
-  get 'albums/show'
+  resources :homes, only: [:index]
 
-  get 'albums/new'
-
-  get 'albums/create'
-
-  get 'albums/edit'
-
-  get 'albums/update'
-
-  get 'albums/destroy'
-
-  get 'books/index'
-
-  get 'books/show'
-
-  get 'books/new'
-
-  get 'books/create'
-
-  get 'books/edit'
-
-  get 'books/update'
-
-  get 'books/destroy'
-
-  get 'movies/index'
-
-  get 'movies/show'
-
-  get 'movies/new'
-
-  get 'movies/create'
-
-  get 'movies/edit'
-
-  get 'movies/update'
-
-  get 'movies/destroy'
+  resources :movies
+  resources :books
+  resources :albums
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
