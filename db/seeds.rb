66429431b42c5dd3839a21acc54711cc37325710
Rachel <rachel.l.pavilanis@gmail.com>
2016-10-11 14:06:51 -0700

    reseeded and added data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Albums
+Album.create(title: "The Suburbs", recorded_by: "Arcade Fire", ranked: 0, description: "The Suburbs is the third studio album by the Canadian indie rock band Arcade Fire released in August 2010.")
+
+Album.create(title: "Barton Hollow", recorded_by: "The Civil Wars", ranked: 0, description: "Barton Hollow is the first full-length studio album from The Civil Wars.")
+
+Album.create(title: "The Phosphorescent Blues", recorded_by: "Punch Brothers", ranked: 0, description: "The Phosphorescent Blues is the fourth studio album by the American group Punch Brothers, released on January 27, 2015.")
+
+#Movies
