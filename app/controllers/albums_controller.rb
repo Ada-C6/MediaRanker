549ed11dcf897d3fba04a4ff2613d4664895b04f
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-11 17:06:54 -0700

    Updated albums controller/model/view with code adapted from movies

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,24 +1,55 @@
 class AlbumsController < ApplicationController
 
   def index
+    @albums = Album.all
   end
 
   def show
+    @album = Album.find(params[:id])
   end
 
   def new
+    @album = Album.new
   end
 
   def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_path(@album.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @album = Album.find(params[:id])
   end
 
   def update
+    @album = Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to album_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @movie = Album.find(params[:id])
+    @movie.destroy
+    redirect_to albums_path
+  end
+
+  def upvote
+    @album = Album.find(params[:id])
+    @album.upvote
+    redirect_to album_path(params[:id])
+  end
+
+private
+
+  def album_params
+    params.require(:album).permit(:name, :artist, :description)
   end
 
 end
