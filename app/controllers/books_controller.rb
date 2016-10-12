7008db29e23884a5e20e27d91ce1196c87a53368
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-12 15:42:41 -0700

    THIS COMPLETES ALL BASIC FUNCTIONALITY FOR BOOKS. NEXT WILL COPY THIS FORMAT TO MOVIES. THEN WILL FIGURE OUT HOW TO ONLY SHOW THE TOP 5 RANKING ITEMS FROM EACH CATEGORY ON THE HOME PAGE. TEST AND DO STYLING.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,20 @@ class BooksController < ApplicationController
         @books = Book.all.order(rank: :desc)
     end
 
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+        @book.rank = 0
+        if @book.save  # successful
+            redirect_to books_path
+        else  # unsuccessful, return to form with instance variables poplulated
+            render :new
+        end
+    end
+
     def upvote
         @book = Book.find(params[:id])
         if @book.upvote_one
@@ -10,5 +24,45 @@ class BooksController < ApplicationController
         else
             #raise error
         end
+    end
+
+
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to book_path(@book)
+        else
+            render :edit
+        end
+    end
+
+    def upvote
+        @book = Book.find(params[:id])
+        if @book.upvote_one
+            redirect_to books_path
+        else
+            #raise error
+        end
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:name, :author, :description)
     end
 end
