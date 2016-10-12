b151ba5d4da9b43b85af95aa95ee058436fde620
Allison Hoke <allisonhoke@gmail.com>
2016-10-12 14:07:53 -0700

    migrated databases for movie, book, album

--- db/migrate/20161012030758_create_books.rb
+++ db/migrate/20161012030758_create_books.rb
@@ -1,7 +1,10 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-
+      t.string :title, null: false
+      t.string :author
+      t.integer :rank
+      t.string :description
       t.timestamps null: false
     end
   end
