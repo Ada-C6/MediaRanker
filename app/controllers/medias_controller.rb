8c94d088abad5918a655cf177568a10dfb411cc6
Shari Meggs <smeggs1@gmail.com>
2016-10-11 17:00:39 -0700

    Migrated a default value for rank in movies

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,7 +1,7 @@
 class MediasController < ApplicationController
   def index
-    @movies = Movie.all
-    @books = Book.all
-    @albums = Album.all
+    @movies = Movie.all.order(:rank).reverse
+    @books = Book.all.order(:rank).reverse
+    @albums = Album.all.order(:rank).reverse
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011181835) do
+ActiveRecord::Schema.define(version: 20161011235115) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 20161011181835) do
     t.string   "name"
     t.integer  "year"
     t.string   "description"
-    t.integer  "rank"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "rank",        default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
 end
