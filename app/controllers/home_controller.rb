23617b8772c7f073eb715c4ae32bde26c925d213
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-12 16:45:52 -0700

    FIGURED OUT HOW TO ONLY SHOW THE TOP 5 RANKING ITEMS FROM EACH CATEGORY ON THE HOME PAGE. Tomorrow will be dedicated to writing tests and styling.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+    def index
+        @albums = Album.all.order(rank: :desc)
+        @books = Book.all.order(rank: :desc)
+        @movies = Movie.all.order(rank: :desc)
+    end
 end
