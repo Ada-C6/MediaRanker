a01e024a7bccb42a12d08ef1dabc1238a4777138
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-12 14:55:37 -0700

    implemented basic root view and controller

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,7 @@
 class Album < ActiveRecord::Base
   validates :name, presence: true
+
+  def self.sort_by_rank
+    order('rank DESC')
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ActiveRecord::Base
   validates :title, presence: true
+
+  def self.sort_by_rank
+    order('rank DESC')
+  end
+
 end
