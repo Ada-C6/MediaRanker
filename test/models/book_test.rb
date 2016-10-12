604ea009f1c73738dbd43fb6aeb6985cb33a589a
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-12 15:41:36 -0700

    added testing gems and wrote book tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Cannot create a book without a title" do
+    book = Book.new
+    assert_not book.valid?
+  end
+
+  test "Create a book with a title" do
+    books.each do |book|
+      assert book.valid?
+    end
+  end
+
+  test "Book.sort_by_rank returns a list of books sorted by rank in descending order" do
+    assert_equal Book.sort_by_rank.first.title, books(:lotr).title
+  end
+
+  test "Book.sort_by_rank places items with nil :rank at the bottom" do
+    assert_equal Book.sort_by_rank.last.title, books(:nil_rank).title
+  end
+
+  test "Book.sort_by_rank returns all items in database" do
+    assert_equal Book.sort_by_rank.count, 4
+  end
 end
