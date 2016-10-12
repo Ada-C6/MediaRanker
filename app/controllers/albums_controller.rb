44e13c2fb37e259d8d7f8f9f9bf742c49da3d0e0
Madeleine Johnson <mjohnson3038@gmail.com>
2016-10-12 16:18:41 -0700

    Books and Albums WIP Update

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,61 @@
 class AlbumsController < ApplicationController
+
+  def index
+    @full_list = Album.all
+  end
+
+  def show
+    @id = Integer(params[:id])
+    @this_item = Album.find(@id)
+  end
+
+  def new
+    @new_item = Album.new
+
+  end
+
+  def create
+    @new_item = Album.new(album_params)
+    @new_item.rank = 0
+    @new_item.save
+    redirect_to albums_path
+  end
+
+  def edit
+    @id = params[:id]
+    @this_item = Album.find(params[:id])
+
+  end
+
+  def update
+    @this_item = Album.find(params[:id])
+    if @this_item.update(album_params)
+      redirect_to edit_album_path
+    else
+      render :edit
+    end
+  end
+
+  def vote
+    id = params[:id]
+    this_item = Album.find(id)
+    this_item.rank +=1
+    this_item.save
+    redirect_to albums_path
+
+  end
+
+  def destroy
+    @item = Album.find(params[:id]).destroy
+    redirect_to albums_path
+  end
+
+end
+
+
+
+private
+
+def album_params
+  params.require(:album).permit(:name, :artist, :description)
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,62 @@
 class BooksController < ApplicationController
 
+  def index
+    @full_list = Book.all
+  end
+
   def show
-    id = Integer(params[:id])
-    @this_book = Book.find(id)
+    @id = Integer(params[:id])
+    @this_item = Book.find(@id)
+  end
+
+  def new
+    @new_item = Book.new
 
   end
+
+  def create
+    @new_item = Book.new(book_params)
+    @new_item.rank = 0
+    @new_item.save
+    redirect_to books_path
+  end
+
+  def edit
+    @id = params[:id]
+    @this_item = Book.find(params[:id])
+
+  end
+
+  def update
+    @this_item = Book.find(params[:id])
+    if @this_item.update(book_params)
+      redirect_to edit_book_path
+    else
+      render :edit
+    end
+  end
+
+  def vote
+    id = params[:id]
+    this_item = Book.find(id)
+    this_item.rank +=1
+    this_item.save
+    redirect_to books_path
+
+  end
+
+  def destroy
+    @item = Book.find(params[:id]).destroy
+    redirect_to books_path
+  end
+
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
+
 end
