ba8f10d22a525d7ffaed2f8874fa39cb14d0b3cb
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-11 18:44:34 -0700

    Added presence of name validatation to Album/Book/Movie models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
-
-  def upvote
-    self.ranking += 1
-    self.save
-  end
+  validates :name, presence: true
+    def upvote
+      self.ranking += 1
+      self.save
+    end
 end
