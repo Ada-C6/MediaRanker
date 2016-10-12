38bfee34624e3583d93e43174b897c87d1927afa
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-11 17:17:00 -0700

    created the book index view with the upvote functionality working, updating the routes, controller and model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,14 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all.order(rank: :desc)
+    end
+
+    def upvote
+        @book = Book.find(params[:id])
+        if @book.upvote_one
+            redirect_to books_path
+        else
+            #raise error
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     patch 'albums/:id/upvote' => 'albums#upvote', as: 'album_upvote'
 
     resources :books
+    patch 'books/:id/upvote' => 'books#upvote', as: 'book_upvote'
 
     resources :movies
 end
