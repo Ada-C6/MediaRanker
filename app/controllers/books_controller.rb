72c2eb2014333b918449ae8e7b4bdf4f3f9f68a0
Shari Meggs <smeggs1@gmail.com>
2016-10-12 16:41:44 -0700

    Added new for books and set defaults for rank

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,13 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
   end
 
   def update
@@ -17,6 +22,15 @@ class BooksController < ApplicationController
       redirect_to book_path
     else
       render :edit
+    end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
     end
   end
 
@@ -32,10 +46,9 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
-
   private
 
-def book_params
-  params.require(:book).permit(:title, :author, :description)
-end
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011235115) do
+ActiveRecord::Schema.define(version: 20161012234005) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
     t.string   "artist"
     t.string   "best_song"
     t.string   "description"
-    t.integer  "rank"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "rank",        default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.string   "description"
-    t.integer  "rank"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "rank",        default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "movies", force: :cascade do |t|
