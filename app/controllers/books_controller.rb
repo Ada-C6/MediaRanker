5c1f2efd16097c191127e4821ea469ccc2fb1ed5
Shari Meggs <smeggs1@gmail.com>
2016-10-12 16:27:21 -0700

    Added edit and show for books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.all.order(:rank).reverse
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      @book.save
+      redirect_to book_path
+    else
+      render :edit
+    end
+  end
+
+  def upvote
+    @book = Book.find(params[:id])
+    @book.rank += 1
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to books_path
+  end
+
+
+  private
+
+def book_params
+  params.require(:book).permit(:title, :author, :description)
+end
+end
