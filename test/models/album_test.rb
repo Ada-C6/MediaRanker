cf1709be568327eefb38fe4cb8bf5712639b9ef3
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-12 16:36:37 -0700

    Added tests of validation coverage for Movie/Book/Album models

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
+
+    test "Create a album with all data" do
+    album = Album.new(name: "Shatter Me", artist: "Lindsey Stirling", description: "Awesome")
+    assert album.valid?
+  end
+
+    test "Create a album with just all required data" do
+    album = Album.new(name: "Shatter Me")
+    assert album.valid?
+  end
+
+  test "Cannot create a album with only unrequired data" do
+    album = Album.new(artist: "Lindsey Stirling", description: "Awesome")
+    assert_not album.valid?
+  end
+
   test "Activating upvote method adds 1 to the album.ranking" do
       album = albums(:black_celebration)
       album.upvote
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
+
+    test "Create a book with all data" do
+    book = Book.create(name: "Beaches of the World", author: "Will Jones", description: "beaches")
+    assert book.valid?
+  end
+
+    test "Create a book with just all required data" do
+    book = Book.create(name: "Beaches of the World")
+    assert book.valid?
+  end
+
+  test "Cannot create a book with only unrequired data" do
+    book = Book.create(author: "Beaches of the World", description: "beaches")
+    assert_not book.valid?
+  end
+
   test "Activating upvote method adds 1 to the book.ranking" do
       book = books(:harry_potter)
       book.upvote
