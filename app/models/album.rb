bf981d19df431774e5d7353f1acbb9f36c94811f
guineveresaenger <guineveresaenger@yahoo.com>
2016-10-12 16:01:45 -0700

    Added similar tests to Book and Movie model tests

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: true
-  validates :vote, numericality: { greater_than_or_equal_to: 0} 
+  validates :vote, allow_nil: true, numericality: { greater_than_or_equal_to: 0}
 
   def upvote
     self.vote += 1
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Create a book with valid data" do
+    book = books(:one)
+    assert book.valid?
+  end
+
+  test "Cannot create an unnamed book" do
+    book = Book.new
+    assert_not book.valid?
+    assert_includes book.errors, :title
+
+  end
+
+  test "Cannot create two books with different names" do
+    book1 = books(:one)
+    book2 = Book.new(title: "Sir David Attenborough")
+
+    assert_not book2.valid?
+  end
+
+  test "Vote attribute must be zero or a positive integer" do
+    book1 = Book.new(title: "Test", vote: -1)
+    book2 = books(:two)
+    book3 = books(:three)
+
+    assert_not book1.valid?
+    assert_not book1.save
+
+    assert book2.valid?
+    assert book3.valid?
+  end
+
+  test "Increases vote by one when upvote is called" do
+    book = books(:two)
+    assert_equal book.upvote, 38
+    assert_not_equal book.upvote, 38
+
+  end
+
 end
