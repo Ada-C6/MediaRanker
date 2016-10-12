7ff624240651ba0b3a77d7705782b83a78840d85
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-12 16:56:36 -0700

    css formmating, movie show button

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  validates :name, presence: true
+
+
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Create a movie w/ valid data" do
+    assert movies(:charlottes_web).valid?
+  end
+
+  test "Create an album with no name" do
+    movie = Movie.new
+    assert_not movie.valid?
+  end
 end
