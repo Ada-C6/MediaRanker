e1db5af05b03b33e9453c3c5c047482a0abd6dc5
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-11 19:17:17 -0700

    ALBUM: created edit view and updated controller edit/update methods for functionality. Also added destroy method to the controller and updated the delete link/button on the show view. THIS COMPLETES ALL BASIC FUNCTIONALITY FOR ALBUMS. NEXT WILL COPY THIS FORMAT TO BOOKS AND MOVIES. THEN WILL FIGURE OUT HOW TO ONLY SHOW THE TOP 5 RANKING ITEMS FROM EACH CATEGORY ON THE HOME PAGE. TEST AND DO STYLING.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -21,6 +21,19 @@ class AlbumsController < ApplicationController
         @album = Album.find(params[:id])
     end
 
+    def edit
+        @album = Album.find(params[:id])
+    end
+
+    def update
+        @album = Album.find(params[:id])
+        if @album.update(album_params)
+            redirect_to album_path(@album)
+        else
+            render :edit
+        end
+    end
+
     def upvote
         @album = Album.find(params[:id])
         if @album.upvote_one
@@ -28,6 +41,12 @@ class AlbumsController < ApplicationController
         else
             #raise error
         end
+    end
+
+    def destroy
+        @album = Album.find(params[:id])
+        @album.destroy
+        redirect_to albums_path
     end
 
     private
