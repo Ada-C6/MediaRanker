eb6644c73177d4189d6a6b65e8f7e594aff08336
Emily Abrash <emabrash@gmail.com>
2016-10-13 09:36:42 -0700

    Added controller tests and edited controllers to catch exceptions for missing files

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,11 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    begin
+      @movie = Movie.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render :file => 'public/404.html', :status => :not_found
+    end
   end
 
   def new
@@ -17,42 +21,55 @@ class MoviesController < ApplicationController
     if @movie.save
       redirect_to movies_show_path(@movie.id)
     else
-      render :new
+      render :new, :status => :error
     end
+
   end
 
   def edit
-    @movie = Movie.find(params[:id])
+    begin
+      @movie = Movie.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render :file => 'public/404.html', :status => :not_found
+    end
   end
 
   def update
-    @movie = Movie.find(params[:id])
+    begin
+      @movie = Movie.find(params[:id])
 
-    # Verb patch is used for upvote; verb put is used for full edit
+      # Verb patch is used for upvote; verb put is used for full edit
 
-    if request.patch?
-      @movie.update(upvotes: @movie.upvotes + 1)
-      if request.referer.include?('show')
-        redirect_to movies_show_path(@movie.id)
+      if request.patch?
+        @movie.update(upvotes: @movie.upvotes + 1)
+        if request.referer.include?('show')
+          redirect_to movies_show_path(@movie.id)
+        else
+          redirect_to movies_index_path
+        end
       else
-        redirect_to movies_index_path
-      end
-    else
-      @movie.update_attributes(filter_params)
+        @movie.update_attributes(filter_params)
 
-      if @movie.save
-        redirect_to movies_show_path(@movie.id)
-      else
-        @movie.restore_attributes
-        render :edit
+        if @movie.save
+          redirect_to movies_show_path(@movie.id)
+        else
+          @movie.restore_attributes
+          render :edit, :status => :error
+        end
       end
+    rescue ActiveRecord::RecordNotFound
+      render :file => 'public/404.html', :status => :not_found
     end
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    redirect_to movies_index_path
+    begin
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+      redirect_to movies_index_path
+    rescue ActiveRecord::RecordNotFound
+      render :file => 'public/404.html', :status => :not_found
+    end
   end
 
   def filter_params
