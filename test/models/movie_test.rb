82eb25a245e856703621f688a1ce555ec13e54bd
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-13 00:56:12 -0700

    wrote first test for movies

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Cannot create a movie without a name" do
+    movie = Movie.new
+    assert_not movie.valid?
+    assert_not movies(:three).valid?
+  end
+
+  #test cannot create two books of the same name?
+
 end
