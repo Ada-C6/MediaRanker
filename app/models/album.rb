3de1df61c2d404a0a3448e22e2e067d8062fe52c
Sarah Nahmias <nahmisa@gmail.com>
2016-10-13 14:49:57 -0700

    Beigning of tests

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,11 @@
 class Album < ActiveRecord::Base
+  validates :name, presence: true, uniqueness:
+    { scope:   :artist,
+    message: "This album has already been added" }
+  validates :artist, presence: true
+  validates :description, presence: true, length:
+    { maximum: 1000,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :upvotes, numericality: { only_integer: true }
+
 end
