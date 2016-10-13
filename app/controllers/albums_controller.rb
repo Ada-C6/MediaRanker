a4ce84ed52d7c228d8de86b1b751601b3996fe7c
Allison Hoke <allisonhoke@gmail.com>
2016-10-13 15:28:10 -0700

    copied movies controller actions and adjusted for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,48 @@
 class AlbumsController < ApplicationController
+  def index
+    @albums = Album.all
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    @album.rank = 0
+    if @album.save
+      redirect_to album_path(@album)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to album_path(@album)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id]).destroy
+
+    redirect_to movies_path
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :artist, :description)
+  end
 end
