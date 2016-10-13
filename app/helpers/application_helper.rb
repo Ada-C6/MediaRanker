06d4f535896a05c525f29dcee7e56c6917689920
Sarah Nahmias <nahmisa@gmail.com>
2016-10-12 20:50:29 -0700

    Refacors paths to work across different kinds of media
    Uses helper methods to make partials work across media

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def create_show_path(media)
+
+    if media.class == Movie
+       movie_path(media)
+    elsif media.class == Book
+      book_path(media)
+    elsif media.class == Album
+      album_path(media)
+    end
+  end
+
+  def get_creator(media)
+    if media.class == Movie
+      media.director
+    elsif media.class == Book
+      media.author
+    elsif media.class == Album
+      media.artist
+    end
+  end
+
 end
