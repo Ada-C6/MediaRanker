054f100a8045d6ed77b21817aab356867b5ee285
Sarah Nahmias <nahmisa@gmail.com>
2016-10-12 21:36:42 -0700

    Creates albums views and actions mirroring books and movies

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,53 @@
 class AlbumsController < ApplicationController
+  def index
+    @albums = Album.order(upvotes: :desc)
+  end
+
   def show
     @album = Album.find(params[:id])
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    # media starts out with zero and not nill upvotes.
+    @album.upvotes = 0
+
+    if @album.save
+      redirect_to album_path(@album)
+    else
+      render :new
+
+    end
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update(album_params)
+      redirect_to album_path(@album)
+    else
+      render :edit
+
+    end
+  end
+
+  def destroy
+    album = Album.find(params[:id])
+    album.destroy
+    redirect_to albums_path
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :artist, :description, :upvotes)
   end
 end
