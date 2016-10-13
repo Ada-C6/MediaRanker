8cf9cd4581fa3a3a50782442ee8bb631398564c1
heatherherrington <redherring@gmail.com>
2016-10-13 16:15:16 -0700

    Minor aesthetic changes

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: { scope: :artist }
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :artist, :message => "This album with this artist has already been created" }
   validates :artist, presence: true
   validates :description, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: { scope: :author }
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :author, :message => "This book with this author has already been created" }
   validates :author, presence: true
   validates :description, presence: true
 end
