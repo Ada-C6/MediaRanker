bde7e640131fa94718acc883903ccee3b169e4e5
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-13 14:56:15 -0700

    added top 10 lists to index, made a partial for display of indexes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,11 @@
 class SiteController < ApplicationController
   def index
-  end 
+    @top_movies = Movie.order(:rank).reverse_order.first(10)
+    # This should be in the movie model.
+    @top_books = Book.order(:rank).reverse_order.first(10)
+
+    @top_albums =
+    Album.order(:rank).reverse_order.first(10)
+
+  end
 end
