58d72a1e65c2e9e6ed53fb1911c47beb5888d7d2
Allison Hoke <allisonhoke@gmail.com>
2016-10-13 14:39:31 -0700

    created new and create actions and new view page for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,24 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.rank = 0
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :director, :description)
+  end
 end
