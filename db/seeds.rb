dac6a7cb61f26efda40d281b9eccc047d459baaa
Yeni <yenicapote08@gmail.com>
2016-10-13 16:53:25 -0700

    created csv files for each table and added to seeds to populate said tables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+
+CSV.foreach('./seed_csvs/albums.csv', :headers => true) do |album|
+  Album.create(album.to_hash)
+end
+
+CSV.foreach('./seed_csvs/books.csv', :headers => true) do |book|
+  Book.create(book.to_hash)
+end
+
+CSV.foreach('./seed_csvs/movies.csv', :headers => true) do |movie|
+  Movie.create(movie.to_hash)
+end
