099d8f312b33aa3ff83d1a348de272c4d47d7885
Sarah Nahmias <nahmisa@gmail.com>
2016-10-13 15:01:19 -0700

    More testing stuff

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,11 +1,21 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  test "Create an empty Album" do
-    album = Album.new
+
+  test "Can create an album with all fields" do
+    album = Album.new(name: 'Harry and the Potters', artist: 'Harry and the Potters', description: 'Harry and the Potters is the eponymous debut studio album by indie rock band Harry and the Potters, released in June 2003.', upvotes: 0)
     assert album.valid?
   end
+
+  test "Cannot create an album with missing fields" do
+    album = Album.new(name: 'Harry and the Potters', artist: 'Harry and the Potters', upvotes: 0)
+    assert_not album.valid?
+  end
+
+  test "An album cannot have nil upvotes" do
+    album = Album.new(name: 'Harry and the Potters', artist: 'Harry and the Potters', description: 'Harry and the Potters is the eponymous debut studio album by indie rock band Harry and the Potters, released in June 2003.')
+    assert_not album.valid?
+  end
+
+
 end
