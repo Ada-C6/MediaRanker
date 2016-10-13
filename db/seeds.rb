14308c5f975fea79f3d9c08e4bac295469a12ac1
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-13 14:58:58 -0700

    remove initial css formating and create homepage grid

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,12 @@ CSV.read('seed_csvs/movies.csv').each do |line|
   Movie.create(movie_hash)
 end
 
-# CSV.read('seed_csvs/vendors.csv').each do |line|
-#   vendor_hash = { ref_id: line[0].to_i, name: line[1], num_employees: line[2].to_i, market_id: line[3].to_i}
-#   Vendor.create(vendor_hash)
-# end
-#
-# CSV.read('seed_csvs/products.csv').each do |line|
-#   product_hash = { ref_id: line[0].to_i, name: line[1], vendor_id: line[2].to_i}
-#   Product.create(product_hash)
-# end
-#
-# CSV.read('seed_csvs/sales.csv').each do |line|
-#   sale_hash = { ref_id: line[0].to_i, amount: line[1].to_i, purchase_time: DateTime.parse(line[2]), vendor_id: line[3].to_i, product_id: line[4].to_i}
-#   Sale.create(sale_hash)
-# end
+CSV.read('seed_csvs/books.csv').each do |line|
+  book_hash = { name: line[0], author: line[1], description: line[2], ranking: line[3].to_i}
+  Book.create(book_hash)
+end
+
+CSV.read('seed_csvs/albums.csv').each do |line|
+  album_hash = { name: line[0], artist: line[1], description: line[2], ranking: line[3].to_i}
+  Album.create(album_hash)
+end
