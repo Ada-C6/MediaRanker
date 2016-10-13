8b2bd17a212cee597c6734d2cdc3abe08ee6a09a
Allison Hoke <allisonhoke@gmail.com>
2016-10-13 14:50:52 -0700

    added edit and update actions for movie and edit view page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,6 +22,19 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def movie_params
