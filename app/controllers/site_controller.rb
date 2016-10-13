d9e9b88edc137c382ef301656308e712ab5b05ca
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-13 15:07:48 -0700

    added links to bottom of index lists

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,7 @@ class SiteController < ApplicationController
   def index
     @top_movies = Movie.order(:rank).reverse_order.first(10)
     # This should be in the movie model.
+    
     @top_books = Book.order(:rank).reverse_order.first(10)
 
     @top_albums =
