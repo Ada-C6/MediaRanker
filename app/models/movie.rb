f2a736fab04a33902d2208d0a7eee3f1db929c3f
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-13 14:49:43 -0700

    modified index view with foundation styling; minor html changes on other pages

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,16 @@ class Movie < ActiveRecord::Base
   def self.sort_by_rank
     order('rank DESC')
   end
+
+  def upvote
+    @movie = Movie.find(params[:id])
+    if @movie.rank.nil?
+      @movie.rank = 1
+    else
+      @movie.rank += 1
+    end
+    @movie.update(movie_params)
+    redirect_to movie_path
+  end
+
 end
