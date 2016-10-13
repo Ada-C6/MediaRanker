58b5efef7043c2c4cd262c7f115393b83b400312
Esther Ng <esther.hc.ng@gmail.com>
2016-10-12 17:00:51 -0700

    Added some basic css. Look into how to access attributes from within model.

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,6 +4,13 @@ class BookTest < ActiveSupport::TestCase
   test "Book cannot be created without a title or author" do
     book = Book.new(description: "Why is there no title or author")
     assert_not book.save
-    assert_includes book.errors, :title && :written_by
+    assert_includes book.errors, :title && :by
   end
+
+  test "Initial ranked should be 0" do
+    book = Book.new(title: "Iliad", by: "Homer")
+    assert_equal book.ranked, 0
+  end
+
+
 end
