3bf9ba68bc602a0ea83e8e08483f620ba679823e
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-13 16:10:24 -0700

    added default values to all rankings

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ActiveRecord::Base
 
 #move the logic for upvoting here?
   # def upvoting
-  #   @book = Book.find(params[:id])
+  #    @book = Book.find(params[:id])
   #   if @book.ranking == nil #from when I had nil values for ranking
   #     @book.ranking = 0
   #     @book.ranking += 1
--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012220859) do
+ActiveRecord::Schema.define(version: 20161013230435) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
     t.string   "artist"
     t.string   "description"
-    t.integer  "ranking"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "ranking",     default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "author"
     t.string   "description"
-    t.integer  "ranking"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "ranking",     default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
     t.string   "director"
     t.string   "description"
-    t.integer  "ranking"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "ranking",     default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
 end
