2c056e4f0d294b933943c9c553811f80a092e59e
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-14 13:20:42 -0700

    Some movies controller tests created/passing; modified show method accordingly

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,11 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    begin
+      @movie = Movie.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => err
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    end
   end
 
   def new
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get the index view" do
+    get :index
+    assert_response :success
+    assert_template :index
+  end
+
+  test "should have all of the movies" do
+    get :index
+    movies =  assigns(:movies)
+
+    assert assigns(:movies)
+    assert_not_nil movies
+    assert_not_empty movies
+  end
+
+  test "show a non-extant movie" do
+    movie_id = 12345
+    assert_raises ActiveRecord::RecordNotFound do
+      Movie.find(movie_id)
+    end
+    get :show, { id: movie_id }
+    assert_response :not_found
+  end
+
+  test "show the requested movie" do
+    movie_id = movies(:top_gun).id
+    get :show, { id: movie_id }
+    assert_response :success
+    assert_template :show
+
+    movie = assigns(:movie)
+    assert_not_nil movie
+    assert_equal movie.id, movie_id
+  end
+
+  test "should get the new form" do
+    get :new
+    assert_response :success
+    assert_template :new
+
+    movie = assigns(:movie)
+    assert_not_nil movie
+    assert_nil movie.id
+  end
+
 end
