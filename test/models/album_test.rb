b8f76a033e532c8557d0d28eb3cf5f81382131fd
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-14 12:19:09 -0700

    Added matching album model tests

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Create an album w/ valid data" do
+    assert albums(:a_hard_days_night).valid?
+  end
+
+  test "Create an album with no name" do
+    album = Album.new
+    assert_not album.valid?
+  end
+
+  test "Create an album with the same name as another" do
+    album1 = Album.create!(name: "foo", artist: "hi")
+    album2 = Album.new(name: "foo")
+    assert album2.valid?
+  end
+
+  test "Create an album with the same name and artist as another" do
+    album1 = Album.create!(name: "foo", artist: "bar")
+    album2 = Album.new(name: "foo", artist: "bar")
+    assert_not album2.valid?
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -19,7 +19,6 @@ class MovieTest < ActiveSupport::TestCase
   test "Create a movie with the same name and director as another" do
     movie1 = Movie.create!(name: "foo", director: "bar")
     movie2 = Movie.new(name: "foo", director: "bar")
-    assert movie1.valid?
     assert_not movie2.valid?
   end
 end
