5f612d2102f94bb2a005520bdc8dfaa9033dc2e3
Melissa Rodriguez <olenellina@gmail.com>
2016-10-14 11:03:02 -0700

    adding clarifying comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+    # This code serves as a catch-all for record not found errors in all controllers
+    # The user will be redirected to a 404 page and an HTTP status code of 404 will be returned.
     rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
     def render_404
