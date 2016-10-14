f057eba2d3d91c75036e048931522559348b6f5f
Sarah Nahmias <nahmisa@gmail.com>
2016-10-14 14:18:13 -0700

    Adds validations from album model to other two

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,15 @@
 class Book < ActiveRecord::Base
+  validates :name, presence: true, uniqueness:
+    { scope: :artist }
+  validates :author, presence: true
+  validates :description, presence: true, length:
+    { maximum: 1000,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :upvotes, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def upvote
     self.upvotes += 1
     self.save
   end
+
 end
