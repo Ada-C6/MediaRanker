3aab7cf3ad6650c1c1510304d89602f2b335ff42
Sarah Nahmias <nahmisa@gmail.com>
2016-10-13 23:07:59 -0700

    Debugs bugs discovered in tests

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,11 +1,11 @@
 class Album < ActiveRecord::Base
   validates :name, presence: true, uniqueness:
-    { scope:   :artist,
-    message: "This album has already been added" }
+    { scope: :artist }
+    # message: "This album has already been added" }
   validates :artist, presence: true
   validates :description, presence: true, length:
     { maximum: 1000,
     too_long: "%{count} characters is the maximum allowed" }
-  validates :upvotes, numericality: { only_integer: true }
+  validates :upvotes, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 end
