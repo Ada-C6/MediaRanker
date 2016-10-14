a46bd23387956f0ed74f55c10c40e991ae2bdc9b
LaurenSky <ljherrer@gmail.com>
2016-10-14 09:03:35 -0700

    Added foundation and css for index pg and movies pgs

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ActiveRecord::Base
   validates :name, presence: true
-  validates :description, allow_nil: true, length: { in: 0..70 }
+  validates :description, allow_nil: true, length: { in: 0..150 }
 end
