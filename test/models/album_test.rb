fea4fa13bda7df90efeecbff4f226dacb6e97e62
Allison Hoke <allisonhoke@gmail.com>
2016-10-15 09:28:22 -0700

    added album data to YAML file and wrote tests for album model

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A valid entry will save" do
+    album = albums(:valid_album)
+
+    assert album.save
+    assert album.valid?
+  end
+
+  test "validations: album artist can't be blank" do
+    album = Album.new(title: "New Album")
+
+    assert_not album.save
+    assert_not album.valid?
+    assert album.errors.keys.include?(:artist), "artist is not in the errors hash"
+  end
+
+  test "validations: album title can't be blank" do
+    album = Album.new(artist: "Allison")
+
+    assert_not album.save
+    assert_not album.valid?
+    assert album.errors.keys.include?(:title), "title is not in the errors hash"
+  end
+
+  test "validations: album title must be unique" do
+    album = albums(:valid_album)
+    assert album.save
+
+    album2 = Album.new(title: "Valid Album", artist: "Allison")
+    assert_not album2.save
+    assert_not album2.valid?
+    assert album2.errors.keys.include?(:title), "title is not in the errors hash"
+  end
+
+  test "#upvote: should increase the rank value by 1" do
+    album = albums(:valid_album)
+
+    assert album.save
+    assert album.upvote
+    assert album.save
+    assert_equal album.rank, 1, "Rank value did not increase by 1"
+  end
 end
