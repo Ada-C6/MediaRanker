fcc828fd3c354805fc16e1a6e5646acc01097231
Nicole Url <nicole.url.4@gmail.com>
2016-10-14 22:36:17 -0700

    Added some more model controller methods

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class AlbumsControllerTest < ActionController::TestCase
-  ### INDEX ###
+  ################## INDEX ##################
   test "Index should display correctly" do
     get :index
     assert_response :success
@@ -14,18 +14,46 @@ class AlbumsControllerTest < ActionController::TestCase
     assert_equal assigns(:albums).count, Album.count
   end
 
-  ### CREATE ###
+  ################## CREATE ##################
+  test "Create should add one to all Albums" do
+    assert_difference("Album.count", 1) do
+      post :create, album: { name: "lkjsdf", artist: "sjkfsa", rank_points: 0 }
+    end
+  end
 
-  ### NEW ###
+  test "Create should redirect to albums#index" do
+    post :create, album: { name: "lkjsdf", artist: "sjkfsa", rank_points: 0 }
+    assert_response :found
+    assert_redirected_to :albums
+  end
 
-  ### EDIT ###
+  ################## NEW ##################
+  test "should get the new form for a new albums" do
+    get :new
+    assert_response :success
+    assert_template :new
 
-  ### SHOW ###
+    album = assigns(:album)
+    assert_not_nil album
+    assert_nil album.id
+  end
 
-  ### UPDATE ###
+  ################## EDIT ##################
+  test "should get the edit form for a new albums" do
+    get :edit
+    assert_response :success
+    assert_template :edit
 
-  ### DESTROY ###
+    album = assigns(:album)
+    assert_not_nil album
+    assert_nil album.id
+  end
+  ################## SHOW ##################
 
-  ### VOTE ###
+  ################## UPDATE ##################
+
+  ################## DESTROY ##################
+
+  ################## VOTE ##################
 
 end
