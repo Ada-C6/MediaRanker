d8ac2f27cb81cf4341f07f1cc3ac54a080a05005
Allison Hoke <allisonhoke@gmail.com>
2016-10-15 09:24:29 -0700

    added book data to the YAML file and wrote tests for the book model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A valid entry will save" do
+    book = books(:valid_book)
+
+    assert book.save
+    assert book.valid?
+  end
+
+  test "validations: book author can't be blank" do
+    book = Book.new(title: "New Book")
+
+    assert_not book.save
+    assert_not book.valid?
+    assert book.errors.keys.include?(:author), "author is not in the errors hash"
+  end
+
+  test "validations: book title can't be blank" do
+    book = Book.new(author: "Allison")
+
+    assert_not book.save
+    assert_not book.valid?
+    assert book.errors.keys.include?(:title), "title is not in the errors hash"
+  end
+
+  test "validations: book title must be unique" do
+    book = books(:valid_book)
+    assert book.save
+
+    book2 = Book.new(title: "Valid Book", author: "Allison")
+    assert_not book2.save
+    assert_not book2.valid?
+    assert book2.errors.keys.include?(:title), "title is not in the errors hash"
+  end
+
+  test "#upvote: should increase the rank value by 1" do
+    book = books(:valid_book)
+
+    assert book.save
+    assert book.upvote
+    assert book.save
+    assert_equal book.rank, 1, "Rank value did not increase by 1"
+  end
 end
