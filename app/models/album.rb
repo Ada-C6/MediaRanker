202bc18d029c4c3dba78bf23fb0a0df71b6aeb7d
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-14 22:07:47 -0700

    Built Album model tests. Refactored Album validations and methods.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,16 +1,20 @@
 class Album < ActiveRecord::Base
-  before_validation :correct_missing_artist, :initial_ranking
+  before_validation :correct_missing_artist
+  before_validation :initial_ranking, on: [:save, :create]
 
   validates :name, presence: :true, uniqueness: :true
   validates :artist, presence: :true
+  validates :ranking, numericality: { greater_than_or_equal_to: 0 }
 
   def correct_missing_artist
-    if self.artist.empty?
+    if self.artist.blank?
       self.artist = "unknown artist"
     end
   end
 
   def initial_ranking
-    self.ranking = 0
+    if self.ranking.blank?
+      self.ranking = 0
+    end
   end
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can create an album with unique name and artist" do
+    album = Album.new(name: "Shiny Happy People Holding Hands", artist: "REM")
+    assert album.valid?
+    assert album.save
+  end
+
+
+  test "Cannot create an album with no name" do
+    album = Album.new
+    assert_not album.valid?
+    assert_not album.save
+    assert_includes album.errors, :name
+  end
+
+  test "Cannot create an album with a duplicate name" do
+    orig_album = Album.create(name: "Sunburn")
+    duplicate_album = Album.new(name: "Sunburn")
+    assert_no_difference('Album.count') do
+      assert_not duplicate_album.valid?
+      assert_not duplicate_album.save
+    end
+  end
+
+  test "Can create an album with no artist; default artist 'unknown artist'" do
+    album = Album.new(name: "Let's Push Things Forward")
+    assert album.valid?
+    assert album.save
+    assert_equal album.artist, "unknown artist"
+  end
+
+  test "Can create an album with a positive initial ranking value" do
+    album = Album.new(name: "La La Latch", ranking: 10)
+    assert album.valid?
+    assert album.save
+  end
+
+  test "Cannot create an album with a negative ranking value" do
+    album = Album.new(name: "Lake Woebegone", ranking: -10)
+    assert_not album.valid?
+    assert_not album.save
+  end
+
+  test "Album created without initial ranking value has ranking: 0" do
+    album = Album.new(name: "Love Me Do")
+    assert album.valid?
+    assert album.save
+    assert_equal album.ranking, 0
+  end
+
+  test "Update does not reset ranking" do
+    album = albums(:album_9)
+    initial_rank = album.ranking
+    album.update(artist: "Not a Pokemon")
+    assert_equal initial_rank, album.ranking
+  end
 end
