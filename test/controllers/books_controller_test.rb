c66fe338eac6af818c29c88219806ee21f174f08
Allison Hoke <allisonhoke@gmail.com>
2016-10-15 10:13:55 -0700

    wrote pos & neg test for the album create action

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -17,5 +17,23 @@ class BooksControllerTest < ActionController::TestCase
       assert_nil book.id
     end
 
+    test "add a new book to the DB" do
+      post_params = { book: { title: "New Book",
+								 author: "Allison" } }
+      assert_difference('Book.count', 1) do
+        post :create, post_params
+      end
 
+      book = assigns(:book)
+      assert_redirected_to book_path(book)
+    end
+
+    test "doesn't add an invalid book to the DB" do
+      post_params = { book: { author: "Allison" } }
+      assert_no_difference('Book.count') do
+        post :create, post_params
+      end
+
+      assert_template :new
+    end
 end
