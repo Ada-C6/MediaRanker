8555b1182ab29688bb488a2afe0775582e0921c0
Allison Hoke <allisonhoke@gmail.com>
2016-10-15 09:59:26 -0700

    created controller test for book action new

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionController::TestCase
-  test "index should show all the artists" do
+  test "index should show all the books" do
       get :index
       assert_response :success
       assert_template :index
     end
+
+    test "new should get the new form for a new book" do
+      get :new
+      assert_response :success
+      assert_template :new
+
+      book = assigns(:book)
+      assert_not_nil book
+      assert_nil book.id
+    end
+
+
 end
