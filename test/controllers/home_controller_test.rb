84c3792ea45eeacf72badd52fa5c233087e6420c
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-15 15:06:26 -0700

    Added Unit Tests for home controller (100% coverage; all passing)

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "(index) should get the home index page" do
+    get :index
+    assert_response :success
+    assert_template :index
+  end
+
+  test "(index) should have all of the movies, books, and albums records" do
+    get :index
+    assert_response :success
+    movies = assigns(:movies)
+    books = assigns(:books)
+    albums = assigns(:albums)
+
+    assert assigns(:movies)
+    assert_not_nil movies
+    assert_not_empty movies
+
+    assert assigns(:books)
+    assert_not_nil books
+    assert_not_empty books
+
+    assert assigns(:albums)
+    assert_not_nil albums
+    assert_not_empty albums
+  end
 end
