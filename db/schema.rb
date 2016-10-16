41d54c288bea15a104d27aa3ac1e82b51371afc3
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-16 11:42:42 -0700

    added migration to default votes to zero for each new album, book and movie object. Schema updated when migration was run.

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013221624) do
+ActiveRecord::Schema.define(version: 20161016180306) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
     t.string   "artist"
     t.string   "description"
-    t.integer  "votes"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "votes",       default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "author"
     t.string   "description"
-    t.integer  "votes"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "votes",       default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
     t.string   "director"
     t.string   "description"
-    t.integer  "votes"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "votes",       default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
 end
