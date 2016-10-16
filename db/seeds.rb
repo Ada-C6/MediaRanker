6772fd05563465c4986608436417c6eb924dbcb6
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-15 19:06:36 -0700

    Expanded seed data: more seeds, all have rand num rankings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-album_names = %w(Jazz Rock Hits Country Hip-Hop).shuffle
-artists = %w(Matchbox20 DepecheMode TheCure P!nk KellyClarkson).shuffle
+album_names = %w(Jazz Rock Hits Country Hip-Hop Blues Instrumentals Soundtracks Metal 80s 90s).shuffle
+artists = %w(Matchbox20 DepecheMode Live P!nk KellyClarkson Creed Phish Tool Korn Train).shuffle
 
 artists.length.times do |i|
-  album_hash = { name: "Best of #{ album_names.pop }", artist: "#{ artists.pop }" }
+  album_hash = { name: "Best of #{ album_names.pop }", artist: "#{ artists.pop }", ranking: rand(0..100) }
   Album.create(album_hash)
 end
 
-book_names = %w(Maps UXDesign Cats Dogs Computers).shuffle
-authors = %w(Kim Karie Tom Steph Chris).shuffle
+book_names = %w(Games UXDesign Cats Dogs Computers Archery Hacking Sailing Fishing Comics ).shuffle
+authors = %w(Kim Karie Tom Steph Chris John Max Joe Bob Bill).shuffle
 
 authors.length.times do |i|
-  book_hash = { name: "Best of #{ book_names.pop }", author: "#{ authors.pop }" }
+  book_hash = { name: "Best of #{ book_names.pop }", author: "#{ authors.pop }", ranking: rand(0..100) }
   Book.create(book_hash)
 end
 
-movie_names = %w(Scream Blood Cats Dogs Notebook).shuffle
-directors = %w(Josh Tara Kat Juha Jason).shuffle
+movie_names = %w(Scream Vampires Cats Dogs Notebook Jaws Fear Tombstone Splash Alien).shuffle
+directors = %w(Josh Tara Kat Juha Jason Steve Jeff Kevin Anne Marie).shuffle
 
 directors.length.times do |i|
-  director_hash = { name: "#{ movie_names.pop }", director: "#{ directors.pop }" }
+  director_hash = { name: "#{ movie_names.pop }", director: "#{ directors.pop }", ranking: rand(0..100) }
   Movie.create(director_hash)
 end
