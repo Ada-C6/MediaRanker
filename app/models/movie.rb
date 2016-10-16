79723a4b82fb6e3d95082db652e4ca58ce4ab26b
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-16 11:49:28 -0700

    testing sort, take 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,9 @@ class Movie < ActiveRecord::Base
   validates :director, presence: true
 
   def self.sort_by_rank
-    order('rank DESC')
+    # order('rank DESC') gets me the behavior I want on
+    # local machine, but doesn't work when deployed to
+    # heroku
+    order(:rank)
   end
 end
