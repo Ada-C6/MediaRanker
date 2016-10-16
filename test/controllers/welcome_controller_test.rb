3455eb266fe6372ee29d4d445ea4ac40e3cb9198
Sarah Nahmias <nahmisa@gmail.com>
2016-10-16 14:03:02 -0700

    Test welcome controller

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
 
+  test "should get index" do
+    get :index
+    assert_response :success
+
+  end
+
+  test "index should display movies, books, and albums" do
+    get :index
+    assert_response :success
+
+    movies = assigns(:movies)
+    books  = assigns(:books)
+    albums = assigns(:albums)
+
+    assert_not_nil movies
+    assert_not_nil books
+    assert_not_nil albums
+  end
 end
