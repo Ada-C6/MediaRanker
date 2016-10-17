399f06b0aa26171969c6c3bda8898e128dad9ecd
Qlwentt <qlwentt@gmail.com>
2016-10-17 09:53:00 -0700

    more heroku stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 50.times do
-# 	Book.create({
-# 		name: Faker::Book.title.titleize, 
-# 		author: Faker::Book.author.titleize, 
-# 		description: "A #{Faker::Book.genre.downcase} book", 
-# 		rank: rand(1..100)
-# 		}
-# 	)
-# end
+50.times do
+	Book.create({
+		name: Faker::Book.title.titleize, 
+		author: Faker::Book.author.titleize, 
+		description: "A #{Faker::Book.genre.downcase} book", 
+		rank: rand(1..100)
+		}
+	)
+end
 
-# 50.times do 
-# 	Album.create(
-# 		{name: "#{Faker::Hacker.adjective.capitalize} #{Faker::Hipster.word.capitalize}", 
-# 		artist: Faker::Book.author.titleize, 
-# 		description: Faker::Hipster.sentence.capitalize, 
-# 		rank: rand(1..100)
-# 		}
-# 	)
-# end
+50.times do 
+	Album.create(
+		{name: "#{Faker::Hacker.adjective.capitalize} #{Faker::Hipster.word.capitalize}", 
+		artist: Faker::Book.author.titleize, 
+		description: Faker::Hipster.sentence.capitalize, 
+		rank: rand(1..100)
+		}
+	)
+end
 
-# 50.times do 
-# 	Movie.create(
-# 		{name: Faker::Company.catch_phrase.titleize, 
-# 		director: Faker::Book.author.titleize, 
-# 		description: Faker::Company.bs.capitalize, 
-# 		rank: rand(1..100)
-# 		}
-# 	)
-# end
+50.times do 
+	Movie.create(
+		{name: Faker::Company.catch_phrase.titleize, 
+		director: Faker::Book.author.titleize, 
+		description: Faker::Company.bs.capitalize, 
+		rank: rand(1..100)
+		}
+	)
+end
