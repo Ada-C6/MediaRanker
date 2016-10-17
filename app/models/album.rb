ea269bc846551297ce76cfd6caf0fc16da22ce9a
Yeni <yenicapote08@gmail.com>
2016-10-17 01:45:38 -0700

    added validations to models. That's rather important.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,6 @@
 class Album < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :artist, presence: true
+  validates :description, presence: true
+  validates :ranking, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
