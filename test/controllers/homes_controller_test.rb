a94fb4869b0736cc405ee6e1f60211420e09d43a
Mary Morrison <marymorrison15@gmail.com>
2016-10-16 17:02:19 -0700

    wrote controller tests for index and new for each media type, and index test for homes controller

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class HomesControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_template :index
+  end
 
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,39 +1,47 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
-  #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
+  test "should go to index" do
+    get :index
+    assert_response :success
+    assert_template :index
+  end
+
+  test "should go to new" do
+    get :new
+    assert_response :success
+    assert_template :new
+  end
+
+
   # test "should get create" do
   #   get :create
   #   assert_response :success
+  #   assert_template :create
   # end
-  #
+
   # test "should get edit" do
   #   get :edit
   #   assert_response :success
+  #   assert_template :edit
   # end
   #
   # test "should get update" do
   #   get :update
   #   assert_response :success
+  #   assert_template :update
   # end
   #
   # test "should get delete" do
   #   get :delete
   #   assert_response :success
+  #   assert_template :delete
   # end
   #
   # test "should get show" do
   #   get :show
   #   assert_response :success
+  #   assert_template :show
   # end
 
 end
