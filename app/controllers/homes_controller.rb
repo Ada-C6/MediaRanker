49360d7f4518b35cdb188f87e98dd2342f518496
Kelly Tran <trankphuong@gmail.com>
2016-10-16 17:53:24 -0700

    Used Foundation Block Grid to style homepage. Used Foundation buttons classes and ids to style buttons. Added semantic HTML tags. Added owl image on hoempage. Added gems in Gemfile and bundle install.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,7 @@
 class HomesController < ApplicationController
   def index
-    # only showing the top 10 ranked items in each category
-    @movies = Movie.all.order(:ranked).reverse.first(10)
-    @books = Book.all.order(:ranked).reverse.first(10)
-    @albums = Album.all.order(:ranked).reverse.first(10)
+    @movies = Movie.all.order(:ranked).reverse
+    @books = Book.all.order(:ranked).reverse
+    @albums = Album.all.order(:ranked).reverse
   end
 end
